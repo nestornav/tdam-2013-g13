98ce2d76544d40f3c6bbc5a34b1ab4c9276bf426
santi147@gmail.com <santi147@gmail.com@4c4c961c-7ab6-2ff0-b880-eaed35bbbded>
2013-09-14 01:09:06 +0000

    adding first tabbed Activity

--- trunk/ProjectoIntegrador_g13/src/com/tdam2013/grupo13/MainActivity.java
+++ trunk/ProjectoIntegrador_g13/src/com/tdam2013/grupo13/MainActivity.java
@@ -3,6 +3,8 @@ package com.tdam2013.grupo13;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        TabHost tabHost=(TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabSpec spec1=tabHost.newTabSpec("Tab 1");
+        spec1.setContent(R.id.tab1);
+        spec1.setIndicator("Tab 1");
+
+        TabSpec spec2=tabHost.newTabSpec("Tab 2");
+        spec2.setIndicator("Tab 2");
+        spec2.setContent(R.id.tab2);
+
+        TabSpec spec3=tabHost.newTabSpec("Tab 3");
+        spec3.setIndicator("Tab 3");
+        spec3.setContent(R.id.tab3);
+
+        tabHost.addTab(spec1);
+        tabHost.addTab(spec2);
+        tabHost.addTab(spec3);
     }
 
 
