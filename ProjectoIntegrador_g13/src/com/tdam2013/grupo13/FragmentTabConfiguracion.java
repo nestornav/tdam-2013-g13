e4a2d7d2cb28d1c7baf0101b3c82dde5cede909b
santi147 <santi147@gmail.com>
2013-09-18 22:39:40 +0000

    Adding fragment configuracion

---
+++ ProjectoIntegrador_g13/src/com/tdam2013/grupo13/FragmentTabConfiguracion.java
@@ -0,0 +1,33 @@
+package com.tdam2013.grupo13;
+
+import android.os.Bundle;
+import android.app.ActionBar.Tab;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.app.ActionBar;
+ 
+public class FragmentTabConfiguracion extends Fragment implements ActionBar.TabListener {
+ 
+    private Fragment mFragment;
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setContentView(R.layout.fragmenttab_configuracion);
+    }
+ 
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        mFragment = new FragmentTabConfiguracion();
+        ft.add(android.R.id.content, mFragment);
+        ft.attach(mFragment);
+    }
+ 
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+        ft.remove(mFragment);
+    }
+ 
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+ 
+    }
+ 
+}
