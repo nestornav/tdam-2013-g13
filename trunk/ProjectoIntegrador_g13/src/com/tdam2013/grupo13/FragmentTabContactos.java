6d66cc6eaa48bf0c9b6e480affeb576625de5eb7
santi147@gmail.com <santi147@gmail.com@4c4c961c-7ab6-2ff0-b880-eaed35bbbded>
2013-09-19 21:06:18 +0000

    adding ui pdf and contactos list fragment

--- trunk/ProjectoIntegrador_g13/src/com/tdam2013/grupo13/FragmentTabContactos.java
+++ trunk/ProjectoIntegrador_g13/src/com/tdam2013/grupo13/FragmentTabContactos.java
@@ -1,39 +1,71 @@
 package com.tdam2013.grupo13;
 
-import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
-import android.app.ActionBar;
- 
-public class FragmentTabContactos extends Fragment implements ActionBar.TabListener {
- 
-    private Fragment mFragment;
- 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Get the view from fragment1.xml
-        getActivity().setContentView(R.layout.fragmenttab_contactos);
-    }
- 
-    public void onTabSelected(Tab tab, FragmentTransaction ft) {
-        // TODO Auto-generated method stub
-        mFragment = new FragmentTabContactos();
-        // Attach fragment1.xml layout
-        ft.add(android.R.id.content, mFragment);
-        ft.attach(mFragment);
-    }
- 
-    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-        // TODO Auto-generated method stub
-        // Remove fragment1.xml layout
-        ft.remove(mFragment);
-    }
- 
-    public void onTabReselected(Tab tab, FragmentTransaction ft) {
-        // TODO Auto-generated method stub
- 
-    }
- 
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class FragmentTabContactos extends ListFragment implements
+		ActionBar.TabListener {
+
+	private Fragment mFragment;
+
+	// Mock de nombres
+	private String[] contactos = { "Alonzo Tavernier", "Julieta Stockstill",
+			"Shona Blessing", "Bruno Paneto", "Fonda Zilnicki",
+			"Oneida Flickinger", "Obdulia Blunt", "Jona Swiney",
+			"Clinton Decamp", "Patricia Hunley", "Brenda Owens", "Lue Wind",
+			"Odessa Moline", "Lou Dargan", "Aron Redfern", "Pa Tierney",
+			"Maryanna Lone", "Shannan Seiber", "Velia Cao", "Gwendolyn Devens", };
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.fragmenttab_contactos, container,
+				false);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+		// Establecemos el Adapter a la Lista del Fragment
+		setListAdapter(new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_list_item_1, contactos));
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+
+		// Mostramos un mensaje con el elemento pulsado
+		Toast.makeText(getActivity(), "Ha pulsado " + contactos[position],
+				Toast.LENGTH_SHORT).show();
+	}
+
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		mFragment = new FragmentTabContactos();
+		ft.add(android.R.id.content, mFragment);
+		ft.attach(mFragment);
+	}
+
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		ft.remove(mFragment);
+	}
+
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		ft.attach(mFragment);
+	}
+
 }
