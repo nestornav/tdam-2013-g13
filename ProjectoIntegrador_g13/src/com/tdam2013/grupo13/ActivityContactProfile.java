8808033c0e70de7634f7a8fcee99f38e46ae3d65
santi147 <santi147@gmail.com>
2013-09-20 03:29:55 +0000

    passing contactName in intent

--- ProjectoIntegrador_g13/src/com/tdam2013/grupo13/ActivityContactProfile.java
+++ ProjectoIntegrador_g13/src/com/tdam2013/grupo13/ActivityContactProfile.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class ActivityContactProfile extends Activity {
@@ -11,9 +12,14 @@ public class ActivityContactProfile extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_activity_contact_profile);
+		setContentView(R.layout.activity_contact_profile);
 		// Show the Up button in the action bar.
 		setupActionBar();
+		
+		String contactName = getIntent().getStringExtra("contactName");
+		
+		Toast.makeText(this, "Nombre del contacto " + contactName,
+				Toast.LENGTH_SHORT).show();
 	}
 
 	/**
