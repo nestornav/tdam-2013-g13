853d93f66532ce8ed025d88ee2fa0636f1851f71
santi147 <santi147@gmail.com>
2013-11-25 00:52:32 +0000

    adding base preferences in FragmentTabConfiguracion.java

--- ProjectoIntegrador_g13/src/com/tdam2013/grupo13/FragmentTabConfiguracion.java
+++ ProjectoIntegrador_g13/src/com/tdam2013/grupo13/FragmentTabConfiguracion.java
@@ -1,30 +1,18 @@
 package com.tdam2013.grupo13;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.app.ActionBar.Tab;
 import android.app.Fragment;
-import android.app.FragmentTransaction;
-import android.app.ActionBar;
- 
-public class FragmentTabConfiguracion extends Fragment{
- 
-    private Fragment mFragment;
- 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-//        getActivity().setContentView(R.layout.fragmenttab_configuracion);
-        
-    }
-    
-    @Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragmenttab_configuracion, container,
-				false);
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class FragmentTabConfiguracion extends PreferenceFragment {
+
+	private Fragment mFragment;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Load the preferences from an XML resource
+		addPreferencesFromResource(R.xml.preferences);
 	}
- 
 }
