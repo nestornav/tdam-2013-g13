14273fa1c20c2af9719d200a392397e8fa7543ac
nestornav@gmail.com <nestornav@gmail.com@4c4c961c-7ab6-2ff0-b880-eaed35bbbded>
2013-11-26 04:19:16 +0000

    Add insert query in the DataBaseManager class.

--- trunk/ProjectoIntegrador_g13/src/com/tdam2013/grupo13/dataBase/DataBaseManager.java
+++ trunk/ProjectoIntegrador_g13/src/com/tdam2013/grupo13/dataBase/DataBaseManager.java
@@ -1,5 +1,6 @@
 package com.tdam2013.grupo13.dataBase;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -37,4 +38,39 @@ public class DataBaseManager extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS ConnectivityLog");
         onCreate(db);		
 	}
+	
+	public void insertConnectivityLog(int date, String connectivityType, String state){
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues cv = new ContentValues();
+		
+		cv.put("date",date);
+		cv.put("connectivityType",connectivityType);
+		cv.put("state",state);
+		
+		db.insert("ConnectivityLog", "idConnectivity", cv);		
+		db.close();
+	}
+	
+	public void insertWebClient(String nickname){
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues cv = new ContentValues();
+		
+		cv.put("userName",nickname);
+		
+		db.insert("WebUser", "idWebUser", cv);		
+		db.close();
+	}
+	
+	public void insertNewMessage(String senderName, String receiverName,int date, String message){
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues cv = new ContentValues();
+		
+		cv.put("senderName",senderName);
+		cv.put("receiverName",receiverName);
+		cv.put("date",date);
+		cv.put("message",message);
+		
+		db.insert("WebMessage", "idWebMessage", cv);		
+		db.close();
+	}	
 }
